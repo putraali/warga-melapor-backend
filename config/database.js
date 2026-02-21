ad6f9a5831169578cbe3bb74242e4c857de6de30
putraali <k9724aliputra@gmail.com>
2026-02-21 14:57:54 +0700

    update

--- config/database.js
+++ config/database.js
@@ -7,9 +7,17 @@ const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.D
     dialect: "mysql",
     port: process.env.DB_PORT,
     
-    // --- TAMBAHAN PENTING ---
-    timezone: '+07:00', // Set ke WIB (agar tanggal tidak geser ke UTC)
-    logging: console.log, // Agar kita bisa lihat proses ALTER TABLE di terminal
+    // --- PENYESUAIAN KRUSIAL UNTUK CLOUD (AIVEN) ---
+    dialectOptions: {
+        ssl: {
+            require: true, // Mewajibkan koneksi terenkripsi
+            rejectUnauthorized: false // Mengizinkan koneksi SSL tanpa melampirkan file sertifikat CA fisik
+        }
+    },
+
+    // --- KONFIGURASI LOKALISASI & DEBUGGING ---
+    timezone: '+07:00', // Set ke WIB (agar tanggal createdAt/updatedAt akurat)
+    logging: console.log, // Menampilkan query SQL di terminal log Railway
 });
 
 export default db;
